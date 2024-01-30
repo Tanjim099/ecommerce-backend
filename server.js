fe11c08422abc29c041b18dae38339d1aac807b1
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-01-30 14:59:48 +0530

    updated

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(
     cors({
         origin: "https://ecommerce-app-liart-three.vercel.app/",
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         optionsSuccessStatus: 204,
     })
